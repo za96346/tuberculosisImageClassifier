62d447c6ee42fdaa81fc022e07f2dba3566c6570
siou <za96346@gmail.com>
2024-09-29 13:05:38 +0800

    chore: 加上gpu 使用

--- index.py
+++ index.py
@@ -6,6 +6,16 @@ from sklearn.model_selection import train_test_split
 from keras.api.applications import VGG16
 from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
 
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.set_visible_devices(gpus[0], 'GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)  # 可選：設置為按需使用內存
+    except RuntimeError as e:
+        print(e)
+
 data_dir = 'TB_Chest_Radiography_Database/'  # 数据主目录
 classes = ['Normal', 'Tuberculosis']  # 类别名
 image_paths = []
