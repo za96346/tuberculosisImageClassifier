5dcd5b52807cfd4e3ae0f2647c2e29a89ef8dc47
siou <za96346@gmail.com>
2024-10-23 13:35:52 +0800

    chore: vgg16加上模型建構

--- VGG16/index.py
+++ VGG16/index.py
@@ -1,6 +1,55 @@
 from Interface import BaseModel
+from keras.api.models import Sequential
+from keras.api.metrics import AUC, Accuracy, F1Score, PrecisionAtRecall
+from keras.api.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
+import keras_cv
 
 
 class ModelImplement(BaseModel):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, input_shape):
+        super().__init__(input_shape)
+        self.model = Sequential([
+            Conv2D(64, (3, 3), input_shape=input_shape, padding='same',
+                activation='relu'),
+            Conv2D(64, (3, 3), activation='relu', padding='same'),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            Conv2D(128, (3, 3), activation='relu', padding='same'),
+            Conv2D(128, (3, 3), activation='relu', padding='same',),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            Conv2D(256, (3, 3), activation='relu', padding='same',),
+            Conv2D(256, (3, 3), activation='relu', padding='same',),
+            Conv2D(256, (3, 3), activation='relu', padding='same',),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            Conv2D(512, (3, 3), activation='relu', padding='same',),
+            Conv2D(512, (3, 3), activation='relu', padding='same',),
+            Conv2D(512, (3, 3), activation='relu', padding='same',),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            Conv2D(512, (3, 3), activation='relu', padding='same',),
+            Conv2D(512, (3, 3), activation='relu', padding='same',),
+            Conv2D(512, (3, 3), activation='relu', padding='same',),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            Flatten(),
+            Dense(4096, activation='relu'),
+            Dense(4096, activation='relu'),
+            Dense(1, activation='sigmoid')
+        ])
+
+        # 編譯模型時確保 metrics 使用正確的參數
+        self.model.compile(
+            optimizer='adam',
+            loss=keras_cv.losses.FocalLoss(from_logits=False),  # 如果你的輸出是概率
+            metrics=[
+                AUC(
+                    num_thresholds=200,
+                    curve="ROC",
+                    summation_method="interpolation",
+                ),
+                Accuracy(),
+                F1Score(average='micro'),  # 適合二元分類
+                PrecisionAtRecall(0.5, num_thresholds=200)  # 設定適當的 threshold
+            ]
+        )
+
+
+        self.model.summary()
+        
