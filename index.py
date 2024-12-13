13f771043053b0114a588880be90cd6ece663671
siou <za96346@gmail.com>
2024-12-13 05:40:46 +0000

    chore: 調小learning rate

--- index.py
+++ index.py
@@ -32,7 +32,7 @@ for modelName, modelImplement in models.items():
         )
 
         if modelConfig[modelName]["traning"] and model:
-            model.startTraining(5, 20, 64, 1e-6)
+            model.startTraining(5, 20, 64, 0.001)
 
         if modelConfig[modelName]["plotTrainingHistory"]:
             model.plotTrainingHistory()
