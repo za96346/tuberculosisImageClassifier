b159ef98c676a04f6e1e46d3b262d71bf38309e7
siou <za96346@gmail.com>
2024-12-13 05:38:40 +0000

    chore: 調整 loss function

--- Interface/BaseModel.py
+++ Interface/BaseModel.py
@@ -101,7 +101,8 @@ class BaseModel(ModelInterface):
             # 编译模型时确保 metrics 使用正确的参数
             model.compile(
                 optimizer=Adam(learning_rate=learning_rate),
-                loss=keras_cv.losses.FocalLoss(gamma=2., alpha=0.25),  # 如果你的输出是概率
+                # loss=keras_cv.losses.FocalLoss(gamma=2., alpha=0.25),  # 如果你的输出是概率
+                loss='binary_crossentropy',
                 metrics=[
                     AUC(num_thresholds=200, curve="ROC",
                         summation_method="interpolation"),
