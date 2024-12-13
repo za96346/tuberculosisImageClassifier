3f6d83eaa7e402bcdef5b4191d616a618afee6db
siou <za96346@gmail.com>
2024-12-13 06:30:20 +0000

    refactor: 重構 baseModel 的資料處理

--- Interface/BaseModel.py
+++ Interface/BaseModel.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from glob import glob
 import gc
+import pandas as pd
 from .ModelInterface import ModelInterface
 import tensorflow as tf
 from sklearn.model_selection import KFold
@@ -12,6 +13,7 @@ import matplotlib.pyplot as plt
 from keras.api.optimizers import Adam
 from keras.api.metrics import AUC, Accuracy, F1Score, PrecisionAtRecall
 import keras_cv
+from sklearn.preprocessing import LabelEncoder
 from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
 
 
@@ -50,51 +52,66 @@ class BaseModel(ModelInterface):
     def loadModel(self):
         pass
 
+    def imagePreprocess(self):
+        # 掃描目錄中的圖像路徑和標籤
+        all_image_paths = []
+        all_labels = []
+
+        for class_name in os.listdir(self.datasetsDir):
+            class_dir = os.path.join(self.datasetsDir, class_name)
+            if os.path.isdir(class_dir):
+                for img_name in os.listdir(class_dir):
+                    img_path = os.path.join(class_dir, img_name)
+                    all_image_paths.append(img_path)
+                    all_labels.append(class_name)
+
+        all_image_paths = np.array(all_image_paths)
+        all_labels = np.array(all_labels)
+
+        print("all_image_paths length => ", len(all_image_paths))
+        print("all_labels length => ", len(all_labels))
+        
+        print("all_image_paths =>", all_image_paths)
+        print("all_labels =>", all_labels)
+
+        return all_image_paths, all_labels
+
 
     def startTraining(self, num_folds, epochs, batch_size, learning_rate):
         self.batch_size = batch_size
 
-        # 使用 ImageDataGenerator 預處理影像
-        datagen = ImageDataGenerator(
-            rescale=1.0/255,  # 將像素值歸一化到 [0, 1]
-            validation_split=0.2  # 保留 20% 作為測試集
-        )
-
-        # 建立數據生成器
-        train_generator = datagen.flow_from_directory(
-            self.datasetsDir,
-            target_size=(self.imageSize[0], self.imageSize[1]),
-            batch_size=batch_size,
-            class_mode='categorical',
-            subset='training',
-            shuffle=True
-        )
+        all_image_path, all_labels = self.imagePreprocess()
 
         # KFold 交叉驗證
-        kf = KFold(n_splits=num_folds, shuffle=True)
+        kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
 
-        # 將數據提取為 numpy 數組
-        x_data = []
-        y_data = []
-
-        for i in range(len(train_generator)):
-            x, y = train_generator[i]
-            y = np.argmax(y, axis=1).reshape(-1, 1)
-            x_data.append(x)
-            y_data.append(y)
-
-        x_data = np.concatenate(x_data, axis=0)
-        y_data = np.concatenate(y_data, axis=0)
-
-        fold_no = 1
         allHistory = {}
-        scores = []
-        for train_index, val_index in kf.split(x_data):
-            print(f'正在訓練第 {fold_no} 折...')
+        for  fold, (train_index, val_index) in enumerate(kf.split(all_image_path)):
+            print(f'正在訓練第 {fold} 折...')
 
             # 分割訓練集與驗證集
-            X_train, X_val = x_data[train_index], x_data[val_index]
-            y_train, y_val = y_data[train_index], y_data[val_index]
+            train_paths, val_paths = all_image_path[train_index], all_image_path[val_index]
+            train_labels, val_labels = all_labels[train_index], all_labels[val_index]
+
+            datagen = ImageDataGenerator(rescale=1./255)
+
+            train_generator = datagen.flow_from_dataframe(
+                dataframe=pd.DataFrame({'filename': train_paths, 'class': train_labels}),
+                x_col='filename',
+                y_col='class',
+                target_size=(self.imageSize[0], self.imageSize[1]),
+                batch_size=batch_size,
+                class_mode='categorical'
+            )
+            
+            val_generator = datagen.flow_from_dataframe(
+                dataframe=pd.DataFrame({'filename': val_paths, 'class': val_labels}),
+                x_col='filename',
+                y_col='class',
+                target_size=(self.imageSize[0], self.imageSize[1]),
+                batch_size=batch_size,
+                class_mode='categorical'
+            )
 
             model = self.createModel()
 
@@ -113,26 +130,21 @@ class BaseModel(ModelInterface):
 
             model.summary()
 
-            print("X_train => ", X_train)
-            print("Y_train => ", y_train)
-
             history = model.fit(
-                X_train,
-                y_train,
-                validation_data=(X_val, y_val),
+                train_generator,
+                validation_data=val_generator,
                 epochs=epochs,
-                batch_size=batch_size
+                batch_size=batch_size,
+                steps_per_epoch=len(train_generator),
+                validation_steps=len(val_generator)
             )
 
-            allHistory[fold_no] = history.history
-
-            scores.append(model.evaluate(X_val, y_val, verbose=0))
+            allHistory[fold] = history.history
 
             # 每次訓練完成後可選擇保存模型
-            model.save(f'{self.modelSavePath}/model_fold_{fold_no}.h5')
+            model.save(f'{self.modelSavePath}/model_fold_{fold}.h5')
 
-            print(f'第 {fold_no} 折完成')
-            fold_no += 1
+            print(f'第 {fold} 折完成')
 
             # 清除 gpu 佔用
             tf.keras.backend.clear_session()
@@ -143,8 +155,6 @@ class BaseModel(ModelInterface):
 
         with open(f'{self.modelSavePath}/training_history.json', 'w') as json_file:
             json.dump(allHistory, json_file, indent=4)
-        with open(f'{self.modelSavePath}/training_score.json', 'w') as json_file:
-            json.dump(scores, json_file, indent=4)
 
     def plotTrainingHistory(self):
         # Define file paths
